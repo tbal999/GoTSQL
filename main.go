c7269fdb6c4d54cde46ba238f1107a1862494d1e
Tom B <53711814+tbal999@users.noreply.github.com>
2020-06-07 11:42:55 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,139 +1,193 @@
-package backendsql
+package main
 
 import (
-	"context"
-	"database/sql"
 	"fmt"
+	"gosqlwebapp/backendsql"
+	"gosqlwebapp/front"
+	"io"
 	"log"
-	"reflect"
-	"strconv"
-	"strings"
-
-	_ "github.com/denisenkom/go-mssqldb"
+	"net/http"
+	"os"
+	"text/template"
 )
 
-//Pointer to DB struct
-var db *sql.DB
+var mapage = `<title>GO QL - Front-end SQL</title>
+</head>
 
-//SQLquery - Submit a SQL username/password/access details (access) and query (q) and receive three outputs: number of rows, a 1D slice of query result, error.
-func SQLquery(access, q string) (int, []string, error) {
-	//connString := `Server=localhost;Database=master;Trusted_Connection=True;`
-	var err error
-	db, err = sql.Open("sqlserver", access)
-	if err != nil {
-		log.Fatal("Error creating connection pool: ", err.Error())
-	}
-	ctx := context.Background()
-	err = db.PingContext(ctx)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("Connected!\n")
-	count, result, err := read(q)
-	return count, result, err
+<body>
+    <style>
+        input {
+            display: inline-block;
+            float: left;
+            margin-right: 20px;
+            background-color: #00FFFF
+        }
+    </style>
+    <a href="https://imgbb.com/"><img src="https://i.ibb.co/dDg4kWH/images.png" alt="SQL-LOGO" border="0"></a>
+    <p><b>GO QL (Go) - Front-end SQL interface</b></p>
+    <p>GO QL</p>
+    <p>A simple front-end interface for TSQL.</p>
+    <form action="/ui/start" method="POST">
+        <p> <input type="text" name="sqlLogin" placeholder="Server=localhost;Database=master;Trusted_Connection=True;" size="50"><label> <- Type in the SQL login details here (i.e Server=localhost;Database=master;Trusted_Connection=True;)</label></p>
+		<p> <input type="text" name="tablequery" placeholder="table name" size="50"><label> <- Type in table name here to grab column information for queries</label></p>
+		<textarea name="sqlQuery" cols="90" rows="20" placeholder"Type your SQL query here">{{.SqlQuery}}</textarea>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<textarea name="output" cols="90" rows="20">{{.Output}}</textarea><br />
+        <br>
+		<button type="submit" style="background-color: #00FFFF;" value="submitquery">Submit query</button>
+		</form>
+
+		 <form action="/ui/save" method="POST">
+		 <input type="text" name="outputname" placeholder="filename" size="50"><label><- Type name of the CSV file you want to save</label><p>
+		<button type="submit" style="background-color: #00FFFF;" value="saveoutput">Save output to CSV</button>
+		{{.Outputresults}}
+		</form>
+
+	 <br/>
+    <br>
+</body>`
+
+//PageVariables - GUI variables that change on webpages.
+type PageVariables struct {
+	Output        string
+	SqlQuery      string
+	Outputresults string
 }
 
-//read takes in a TSQL query and passes it to a TSQL server using the go-mssqldb driver.
-func read(query string) (int, []string, error) {
-	result := []string{}
-	ctx := context.Background()
-	err := db.PingContext(ctx)
-	if err != nil {
-		return -1, result, err
+var store string
+
+//EnsureDir - Ensures that a DIR exists
+func ensureDir(dirName string) error {
+	err := os.MkdirAll(dirName, os.ModeDir)
+	if err == nil || os.IsExist(err) {
+		return nil
+	} else {
+		return err
 	}
-	rows, err := db.QueryContext(ctx, query)
-	if err != nil {
-		return -1, result, err
-	}
-	defer rows.Close()
-	var count int
-	cols, err := rows.Columns()
-	if err != nil {
-		return -1, result, err
-	}
-	columnHeaders := strings.Join(cols, ",")
-	firstrow := true
-	for rows.Next() {
-		if firstrow == true {
-			result = append(result, columnHeaders)
-			result = append(result, grabrows(*rows))
-			firstrow = false
-		} else {
-			result = append(result, grabrows(*rows))
-		}
-		count++
-	}
-	return count, result, nil
 }
 
-//grabrows takes the type *sql.Rows interface and uses reflection to collect the items.
-//The field of this interface we need is a '<[]driver.Value Value>' and contains the row values so this is where we want to collect the rows.
-//For further info check the sql docs.
-func grabrows(r interface{}) string {
-	text := ""
-	reflection := reflect.ValueOf(r)
-	for i := 0; i < reflection.NumField(); i++ {
-		if reflection.Field(i).String() == "<[]driver.Value Value>" {
-			x := reflection.Field(i)
-			for ii := 0; ii < x.Len(); ii++ {
-				if ii != x.Len()-1 {
-					text += typeswitch(x.Index(ii), false)
-				} else {
-					text += typeswitch(x.Index(ii), true)
-				}
-			}
-		}
+//Writes to a file
+func writeToFile(filename string, data string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
 	}
-	return text
+	defer file.Close()
+
+	_, err = io.WriteString(file, data)
+	if err != nil {
+		return err
+	}
+	return file.Sync()
 }
 
-//typeswitch returns a string that you append to the 'text' variable in the grabrows function.
-//It switches on reflect.Value type and converts the item into a string to be returned.
-func typeswitch(x reflect.Value, lastitem bool) string {
-	var text = ""
-	switch lastitem {
-	case false:
-		switch x.IsNil() {
-		case false:
-			switch x.Elem().Type().String() {
-			case "string":
-				text += x.Elem().String() + ","
-			case "bool":
-				text += strconv.FormatBool(x.Elem().Bool()) + ","
-			case "time.Time":
-				text += "(Change_to_VARCHAR)," //Reflecting to time.Time value doesn't seem to work as far as I can see.
-				//timestring, _ := reflect.ValueOf(x.Index(ii).Elem()).Interface().(time.Time)
-				//fmt.Println(timestring)
-			case "int64":
-				text += strconv.FormatInt(x.Elem().Int(), 10) + ","
-			case "float64":
-				text += strconv.FormatFloat(x.Elem().Float(), 'f', 6, 64) + ","
-			default:
-				text += x.Elem().Type().String() + "_type," //Pick up any other types here.
-			}
-		case true:
-			text += ","
-		}
-	case true:
-		switch x.IsNil() {
-		case false:
-			switch x.Elem().Type().String() {
-			case "string":
-				text += x.Elem().String()
-			case "bool":
-				text += strconv.FormatBool(x.Elem().Bool())
-			case "time.Time":
-				text += "(Change_to_VARCHAR)," //Reflecting to time.Time value doesn't seem to work as far as I can see.
-				//timestring, _ := reflect.ValueOf(x.Index(ii).Elem()).Interface().(time.Time)
-				//fmt.Println(timestring)
-			case "int64":
-				text += strconv.FormatInt(x.Elem().Int(), 10)
-			case "float64":
-				text += strconv.FormatFloat(x.Elem().Float(), 'f', 6, 64)
-			default:
-				text += x.Elem().Type().String() + "_type" //Pick up any other types here.
-			}
-		}
+//Main page handler - the front page.
+func mainpage(w http.ResponseWriter, r *http.Request) {
+	t, err := template.New("mainpage").Parse(mapage)
+	if err != nil { // if there is an error
+		log.Print("template executing error: ", err) //log it
 	}
-	return text
+	HomePageVars := PageVariables{}
+	err2 := t.Execute(w, HomePageVars)
+	if err2 != nil { // if there is an error
+		log.Print("template executing error: ", err) //log it
+	}
+}
+
+//Function that handles displaying the SQL results / saving data.
+func start(w http.ResponseWriter, r *http.Request) {
+	store = ""
+	t, err := template.New("mainpage").Parse(mapage)
+	if err != nil { // if there is an error
+		log.Print("template executing error: ", err) //log it
+	}
+	HomePageVars := PageVariables{}
+	r.ParseForm()
+	sqlLogin := ""
+	sqlQuery := ""
+	if len(r.Form["sqlLogin"]) != 0 && r.Form["sqlLogin"][0] != "" {
+		sqlLogin += r.Form["sqlLogin"][0]
+	}
+	if len(r.Form["sqlQuery"]) != 0 && r.Form["sqlQuery"][0] != "" { //tablequery
+		sqlQuery += r.Form["sqlQuery"][0]
+		HomePageVars.SqlQuery = r.Form["sqlQuery"][0]
+		count, results, err := backendsql.SQLquery(sqlLogin, sqlQuery)
+		if err != nil {
+			HomePageVars.Output = "Error: \n" + err.Error()
+			err22 := t.Execute(w, HomePageVars)
+			if err22 != nil { // if there is an error
+				log.Print("template executing error: ", err) //log it
+			}
+			return
+		}
+		fmt.Printf("Sucessfully pulled %d rows.\n", count)
+		text := ""
+		for index := range results {
+			text += results[index] + "\n"
+		}
+		store = text
+		HomePageVars.Output = text
+	}
+	if len(r.Form["tablequery"]) != 0 && r.Form["sqlQuery"][0] == "" {
+		sqlQuery += "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'" + r.Form["tablequery"][0] + "'"
+		HomePageVars.SqlQuery = sqlQuery
+		count, results, err := backendsql.SQLquery(sqlLogin, sqlQuery)
+		if err != nil {
+			HomePageVars.Output = "Error: \n" + err.Error()
+			err22 := t.Execute(w, HomePageVars)
+			if err22 != nil { // if there is an error
+				log.Print("template executing error: ", err) //log it
+			}
+			return
+		}
+		fmt.Printf("Sucessfully pulled %d rows.\n", count)
+		text := ""
+		for index := range results {
+			if index != 0 {
+				text += results[index] + "\n"
+			}
+		}
+		store = text
+		HomePageVars.Output = text
+	}
+	err2 := t.Execute(w, HomePageVars)
+	if err2 != nil { // if there is an error
+		log.Print("template executing error: ", err) //log it
+	}
+}
+
+func save(w http.ResponseWriter, r *http.Request) {
+	t, err := template.New("mainpage").Parse(mapage)
+	if err != nil { // if there is an error
+		log.Print("template executing error: ", err) //log it
+	}
+	HomePageVars := PageVariables{}
+	filename := ""
+	r.ParseForm()
+	if len(r.Form["outputname"]) != 0 && r.Form["outputname"][0] != "" {
+		filename += r.Form["outputname"][0] + ".csv"
+		fmt.Println(filename + " has been saved!")
+		writeerror := writeToFile("./outputsaves/"+filename, store)
+		if writeerror != nil {
+			fmt.Println(writeerror)
+		}
+	} else {
+		fmt.Println("Filename was empty")
+		HomePageVars.Outputresults = "...Filename was empty!"
+	}
+	err2 := t.Execute(w, HomePageVars)
+	if err2 != nil { // if there is an error
+		log.Print("template executing error: ", err) //log it
+	}
+}
+
+func main() {
+	ensureDir("outputsaves")
+	http.HandleFunc("/", mainpage)
+	http.HandleFunc("/ui/start", start)
+	http.HandleFunc("/ui/save", save)
+	serverPort := front.LaunchServer()
+	err2 := http.ListenAndServe(serverPort, nil) // setting listening port
+	if err2 != nil {
+		fmt.Println(err2)
+		log.Fatal("ListenAndServe: ", err2)
+	}
 }
