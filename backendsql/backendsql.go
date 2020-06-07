b5f08ffd04df13b11a8206d2c68834bf5d687fb9
Tom B <53711814+tbal999@users.noreply.github.com>
2020-06-07 11:40:12 +0100

    Update backendsql.go

--- backendsql/backendsql.go
+++ backendsql/backendsql.go
@@ -8,99 +8,32 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
-	"time"
 
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
+//Pointer to DB struct
 var db *sql.DB
 
-func getField(v interface{}, field int) string {
-	r := reflect.ValueOf(v)
-	f := reflect.Indirect(r).Field(field)
-	fieldValue := f.Interface()
-
-	switch v := fieldValue.(type) {
-	case int64:
-		return strconv.FormatInt(v, 10)
-	case int32:
-		return strconv.FormatInt(int64(v), 10)
-	case int:
-		return strconv.FormatInt(int64(v), 10)
-	case string:
-		return v
-	case bool:
-		if v {
-			return "true"
-		}
-		return "false"
-	case time.Time:
-		return v.String()
-	default:
-		return ""
+//SQLquery - Submit a SQL username/password/access details (access) and query (q) and receive three outputs: number of rows, a 1D slice of query result, error.
+func SQLquery(access, q string) (int, []string, error) {
+	//connString := `Server=localhost;Database=master;Trusted_Connection=True;`
+	var err error
+	db, err = sql.Open("sqlserver", access)
+	if err != nil {
+		log.Fatal("Error creating connection pool: ", err.Error())
 	}
-}
-
-type Something struct {
-	T *time.Time
-}
-
-func grabrows(r interface{}) string {
-	//Scanner := bufio.NewScanner(os.Stdin)
-	text := ""
-	reflection := reflect.ValueOf(r)
-	for i := 0; i < reflection.NumField(); i++ {
-		//fmt.Println(reflection.Field(i).Kind())
-		if reflection.Field(i).String() == "<[]driver.Value Value>" {
-			x := reflection.Field(i)
-			for ii := 0; ii < x.Len(); ii++ {
-				if ii != x.Len()-1 {
-					switch x.Index(ii).IsNil() {
-					case false:
-						switch x.Index(ii).Elem().Type().String() {
-						case "string":
-							text += x.Index(ii).Elem().String() + ","
-						case "bool":
-							text += strconv.FormatBool(x.Index(ii).Elem().Bool()) + ","
-						case "time.Time":
-							text += "(Change_to_VARCHAR)," //Reflecting to time.Time value doesn't seem to work as far as I can see.
-							//timestring, _ := reflect.ValueOf(x.Index(ii).Elem()).Interface().(time.Time)
-							//fmt.Println(timestring)
-						case "int64":
-							text += strconv.FormatInt(x.Index(ii).Elem().Int(), 10) + ","
-						case "float64":
-							text += strconv.FormatFloat(x.Index(ii).Elem().Float(), 'f', 6, 64) + ","
-						default:
-							text += "OTHER_type," //Pick up any other types here.
-						}
-					case true:
-						text += ","
-					}
-				} else {
-					switch x.Index(ii).IsNil() {
-					case false:
-						switch x.Index(ii).Elem().Type().String() {
-						case "string":
-							text += x.Index(ii).Elem().String()
-						case "bool":
-							text += strconv.FormatBool(x.Index(ii).Elem().Bool())
-						case "time.Time":
-							text += "(Change_to_VARCHAR)"
-						case "int64":
-							text += strconv.FormatInt(x.Index(ii).Elem().Int(), 10)
-						case "float64":
-							text += strconv.FormatFloat(x.Index(ii).Elem().Float(), 'f', 6, 64)
-						default:
-							text += "OTHER_type" //Pick up other types here.
-						}
-					}
-				}
-			}
-		}
+	ctx := context.Background()
+	err = db.PingContext(ctx)
+	if err != nil {
+		fmt.Println(err)
 	}
-	return text
+	fmt.Printf("Connected!\n")
+	count, result, err := read(q)
+	return count, result, err
 }
 
+//read takes in a TSQL query and passes it to a TSQL server using the go-mssqldb driver.
 func read(query string) (int, []string, error) {
 	result := []string{}
 	ctx := context.Background()
@@ -123,35 +56,84 @@ func read(query string) (int, []string, error) {
 	for rows.Next() {
 		if firstrow == true {
 			result = append(result, columnHeaders)
-			result = append(result, grabrows(*rows))
+			result = append(result, grabrows(rows))
 			firstrow = false
 		} else {
-			result = append(result, grabrows(*rows))
+			result = append(result, grabrows(rows))
 		}
 		count++
 	}
 	return count, result, nil
 }
 
-func checkError(message string, err error) {
-	if err != nil {
-		log.Fatal(message, err)
+//grabrows takes the type *sql.Rows interface and uses reflection to collect the items.
+//The field of this interface we need is a '<[]driver.Value Value>' and contains the row values so this is where we want to collect the rows.
+//For further info check the sql docs.
+func grabrows(r interface{}) string {
+	text := ""
+	reflection := reflect.ValueOf(r)
+	for i := 0; i < reflection.NumField(); i++ {
+		if reflection.Field(i).String() == "<[]driver.Value Value>" {
+			x := reflection.Field(i)
+			for ii := 0; ii < x.Len(); ii++ {
+				if ii != x.Len()-1 {
+					text += typeswitch(x.Index(ii), false)
+				} else {
+					text += typeswitch(x.Index(ii), true)
+				}
+			}
+		}
 	}
+	return text
 }
 
-func SQLquery(access, q string) (int, []string, error) {
-	//connString := `Server=localhost;Database=master;Trusted_Connection=True;`
-	var err error
-	db, err = sql.Open("sqlserver", access)
-	if err != nil {
-		log.Fatal("Error creating connection pool: ", err.Error())
+//typeswitch returns a string that you append to the 'text' variable in the grabrows function.
+//It switches on reflect.Value type and converts the item into a string to be returned.
+func typeswitch(x reflect.Value, lastitem bool) string {
+	var text = ""
+	switch lastitem {
+	case false:
+		switch x.IsNil() {
+		case false:
+			switch x.Elem().Type().String() {
+			case "string":
+				text += x.Elem().String() + ","
+			case "bool":
+				text += strconv.FormatBool(x.Elem().Bool()) + ","
+			case "time.Time":
+				text += "(Change_to_VARCHAR)," //Reflecting to time.Time value doesn't seem to work as far as I can see.
+				//timestring, _ := reflect.ValueOf(x.Index(ii).Elem()).Interface().(time.Time)
+				//fmt.Println(timestring)
+			case "int64":
+				text += strconv.FormatInt(x.Elem().Int(), 10) + ","
+			case "float64":
+				text += strconv.FormatFloat(x.Elem().Float(), 'f', 6, 64) + ","
+			default:
+				text += x.Elem().Type().String() + "_type," //Pick up any other types here.
+			}
+		case true:
+			text += ","
+		}
+	case true:
+		switch x.IsNil() {
+		case false:
+			switch x.Elem().Type().String() {
+			case "string":
+				text += x.Elem().String()
+			case "bool":
+				text += strconv.FormatBool(x.Elem().Bool())
+			case "time.Time":
+				text += "(Change_to_VARCHAR)," //Reflecting to time.Time value doesn't seem to work as far as I can see.
+				//timestring, _ := reflect.ValueOf(x.Index(ii).Elem()).Interface().(time.Time)
+				//fmt.Println(timestring)
+			case "int64":
+				text += strconv.FormatInt(x.Elem().Int(), 10)
+			case "float64":
+				text += strconv.FormatFloat(x.Elem().Float(), 'f', 6, 64)
+			default:
+				text += x.Elem().Type().String() + "_type" //Pick up any other types here.
+			}
+		}
 	}
-	ctx := context.Background()
-	err = db.PingContext(ctx)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("Connected!\n")
-	count, result, err := read(q)
-	return count, result, err
+	return text
 }
